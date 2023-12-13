1f40ee074ac442f20523bfd9ee4c52a8b4c2fd2e
Joseph Muturi <josephmkaranja07@gmail.com>
2023-12-13 12:38:33 +0300

    trial

---
+++ check_command.c
@@ -0,0 +1,28 @@
+#include "simple_shell.h"
+
+/**
+ * check_commands - check for some arguments
+ * @commands: first argument to check
+ * Return: direction to follow
+ */
+
+size_t check_commands(char **commands)
+{
+	size_t skip = 0, exit_status = 0;
+
+	if (_strcmp("env", commands[0]) == 0)
+	{
+		print_env();
+		skip = 1;
+	}
+	if (skip == 0)
+	{
+		commands[0] = get_path(commands[0]);
+		if (commands[0] != NULL)
+			exit_status = execute(commands);
+		else
+			perror("Path Error");
+		free(commands);
+	}
+	return (exit_status);
+}
