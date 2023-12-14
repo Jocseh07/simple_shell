fb93e163a369a95695f7007baf373a1cfe1bab10
Joseph Muturi <josephmkaranja07@gmail.com>
2023-12-14 09:38:53 +0300

    trial fix 3

--- simple_shell.c
+++ simple_shell.c
@@ -8,7 +8,7 @@
 void simple_shell(void)
 {
 	char *input = NULL, **commands;
-	size_t length = 0, exit_status = 0;
+	size_t length = 0, exit_status = 0, n;
 	ssize_t chars_read = 0;
 
 	while (1)
@@ -29,7 +29,8 @@ void simple_shell(void)
 		}
 		commands = parse_string(input, " ");
 		if (_strcmp("exit", commands[0]) == 0)
-			exit(atoi(commands[1]));
+			n = atoi(commands[1]);
+		exit(n);
 		if (_strcmp("env", commands[0]) == 0)
 		{
 			print_env();
